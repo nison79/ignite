01c86e20328b35d0e5a7b7ae87e4e3b203b3bb92
nison79 <georgenison@gmail.com>
2020-11-19 18:03:21 +0200

    Game detail

--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -14,6 +14,9 @@ const GlobalStyles = createGlobalStyle `
         &::-webkit-scrollbar-thumb {
             background-color: darkblue ;
         }
+        &::-webkit-scrollbar-track {
+        background:white;
+    }
     }
     body {
         
@@ -30,7 +33,7 @@ const GlobalStyles = createGlobalStyle `
     h3{
         font-size:1.3rem;
         color:#333;
-        padding:1.5rem;
+        padding:1.5rem 0rem;
     }
     p{
         font-size:1.2rem;
